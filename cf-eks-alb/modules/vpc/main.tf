123bed809426089c2e3d40caadacaa3d85a9e9d2
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-24 02:13:25 +0200

    updates

--- cf-eks-alb/modules/vpc/main.tf
+++ cf-eks-alb/modules/vpc/main.tf
@@ -37,6 +37,12 @@ resource "aws_subnet" "eks_subnets" {
   )
 }
 
+resource "aws_route_table_association" "private" {
+  count         = length(aws_subnet.eks_subnets)
+  subnet_id     = aws_subnet.eks_subnets[count.index].id
+  route_table_id = element(module.vpc.private_route_table_ids, count.index)
+}
+
 resource "aws_security_group" "alb_sg" {
   name   = "${var.environment}-michel-alb-${var.region}-sg"
   vpc_id = module.vpc.vpc_id
