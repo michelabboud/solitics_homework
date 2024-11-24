61170a7903d0c5ab6addd9fd90187c74e8eef3a7
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-24 10:41:16 +0200

    updates

--- cf-eks-alb/modules/alb/main.tf
+++ cf-eks-alb/modules/alb/main.tf
@@ -14,20 +14,35 @@ module "alb" {
   tags = merge({ Environment = var.environment }, var.tags)
 }
 
-resource "aws_lb_listener" "https" {
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = module.alb.this_lb_arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#
+#   ssl_policy = "ELBSecurityPolicy-2016-08"
+# #   certificate_arn = var.certificate_arn
+#
+#
+#   default_action {
+#     type = "fixed-response"
+#     fixed_response {
+#       content_type = "text/plain"
+#       message_body = "Secure connection to ${var.environment}!"
+#       status_code  = "200"
+#     }
+#   }
+# }
+
+resource "aws_lb_listener" "http" {
   load_balancer_arn = module.alb.this_lb_arn
-  port              = 443
-  protocol          = "HTTPS"
-
-  ssl_policy = "ELBSecurityPolicy-2016-08"
-  # certificate_arn = var.certificate_arn
-
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
     type = "fixed-response"
     fixed_response {
       content_type = "text/plain"
-      message_body = "Secure connection to ${var.environment}!"
+      message_body = "Unsecure connection to ${var.environment}!"
       status_code  = "200"
     }
   }
