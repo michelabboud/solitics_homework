40fac0e0ebb4c2f6e71f1f0641d5ad5dc75b12af
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-24 04:28:19 +0200

    updates

--- cf-eks-alb/modules/eks/main.tf
+++ cf-eks-alb/modules/eks/main.tf
@@ -19,7 +19,6 @@ module "eks_al2" {
   cluster_security_group_id               = aws_security_group.eks_control_plane.id
   node_security_group_id                  = aws_security_group.eks_worker_nodes.id
   cluster_endpoint_private_access         = true
-  source_node_security_group              = true
 
 #   iam_role_arn                    =
 
