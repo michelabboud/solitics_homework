5a136ba0f4447b954cc9cea7444d6dd4ec5e8d43
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-27 21:50:31 +0200

    updates

--- private-link/output.tf
+++ private-link/output.tf
@@ -5,3 +5,11 @@ output "eu-west-3-ami-id"  {
 output "eu-central-1-ami_id" {
   value = data.aws_ami.ubuntu_2404_eu_central-1.id
 }
+
+output "eu-central-1-ami_image_id" {
+  value = data.aws_ami.ubuntu_2404_eu_central-1.image_id
+}
+
+output "eu-central-1-ami_image_location" {
+  value = data.aws_ami.ubuntu_2404_eu_central-1.image_location
+}
