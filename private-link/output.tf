4670e32d787ee2cf2c8fceb6884e352e7e9fec93
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-27 22:03:32 +0200

    updates

--- private-link/output.tf
+++ private-link/output.tf
@@ -2,12 +2,12 @@ output "eu-west-3-ami-id"  {
   value = data.aws_ami.ubuntu_2204_eu_west_3.id
 }
 
-output "eu-central-1-ami_id" {
-  value = data.aws_ami.ubuntu_2204_eu_central-1.id
+output "eu-west-1-ami_image_location" {
+  value = data.aws_ami.ubuntu_2204_eu_west_3.image_location
 }
 
-output "eu-central-1-ami_image_id" {
-  value = data.aws_ami.ubuntu_2204_eu_central-1.image_id
+output "eu-central-1-ami_id" {
+  value = data.aws_ami.ubuntu_2204_eu_central-1.id
 }
 
 output "eu-central-1-ami_image_location" {
