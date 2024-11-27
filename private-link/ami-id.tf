b8c4824e7aa5743930ccae59071a6a066258ddc5
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-27 23:02:03 +0200

    updates

--- private-link/ami-id.tf
+++ private-link/ami-id.tf
@@ -15,3 +15,21 @@ data "aws_ami" "ubuntu_2204_eu_central-1" {
 
   owners = ["099720109477"]
 }
+
+data "aws_ami" "ubuntu_2204_eu_west_3" {
+  provider    = aws.eu-west-3
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
