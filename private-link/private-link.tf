4a808c66e56c364c051384352e18ea58b849e728
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-28 06:57:05 +0200

    updates

--- private-link/private-link.tf
+++ private-link/private-link.tf
@@ -25,7 +25,7 @@ resource "aws_vpc_endpoint" "client_endpoint" {
   subnet_ids         = module.vpc_1.public_subnets
   security_group_ids = [aws_security_group.pvtlnk-sg-1.id]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
   tags = merge({ "Name" = "client-endpoint" }, var.tags)
 
