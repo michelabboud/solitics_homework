a14251b4b2f846e5caa9356760bd4794fe80dd45
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-28 11:19:45 +0200

    updates

--- cf-eks-alb/modules/eks/outputs.tf
+++ cf-eks-alb/modules/eks/outputs.tf
@@ -6,12 +6,12 @@ output "eks_node_groups_name" {
   value = module.eks_al2.eks_managed_node_groups
 }
 
-output "eks_control_plane_sg_id" {
-  description = "The security group ID for the EKS control plane"
-  value       = aws_security_group.eks_control_plane.id
-}
-
-output "eks_worker_nodes_sg_id" {
-  description = "The security group ID for the EKS worker nodes"
-  value       = aws_security_group.eks_worker_nodes.id
-}
+# output "eks_control_plane_sg_id" {
+#   description = "The security group ID for the EKS control plane"
+#   value       = aws_security_group.eks_control_plane.id
+# }
+#
+# output "eks_worker_nodes_sg_id" {
+#   description = "The security group ID for the EKS worker nodes"
+#   value       = aws_security_group.eks_worker_nodes.id
+# }
--- cf-eks-alb/modules/eks/variables.tf
+++ cf-eks-alb/modules/eks/variables.tf
@@ -13,20 +13,20 @@ variable "vpc_cidr" {
   description = "VPC Cidr"
 }
 
-variable "eks_subnets" {
-  type        = list(string)
-  description = "List of subnet cidr for the cluster."
-}
-
-variable "eks_subnet_ids" {
-  type        = list(string)
-  description = "List of subnet IDs for the cluster."
-}
-
-variable "all_subnets" {
-  type        = list(string)
-  description = "List of all VPC subnets cidr."
-}
+# variable "eks_subnets" {
+#   type        = list(string)
+#   description = "List of subnet cidr for the cluster."
+# }
+#
+# variable "eks_subnet_ids" {
+#   type        = list(string)
+#   description = "List of subnet IDs for the cluster."
+# }
+#
+# variable "all_subnets" {
+#   type        = list(string)
+#   description = "List of all VPC subnets cidr."
+# }
 
 variable "environment" {
   type        = string
