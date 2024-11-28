284f5a3142bfd8b90937fe1c0f863cbd54deb44c
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-28 04:01:00 +0200

    updates

--- private-link/output.tf
+++ private-link/output.tf
@@ -42,6 +42,6 @@ output "nlb_zone_id" {
   value = aws_lb.nlb.zone_id
 }
 
-output "vpc_1_public_subnet_ids" {
-  value = [for s in module.vpc_1.public_subnets : s.id]
-}
+# output "vpc_1_public_subnet_ids" {
+#   value = [for s in module.vpc_1.public_subnets : s.id]
+# }
