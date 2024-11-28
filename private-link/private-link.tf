aefdd9e01c732366dfa83ad8bb8a8e3ae4bfa974
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-28 06:04:32 +0200

    updates

--- private-link/private-link.tf
+++ private-link/private-link.tf
@@ -7,18 +7,18 @@ resource "aws_vpc_endpoint_service" "service_endpoint" {
     "arn:aws:iam::${var.account_id}:${var.iam_user}"
   ]
 
-  tags = merge({"Name" = "service-endpoint"}, var.tags)
+  tags       = merge({ "Name" = "service-endpoint" }, var.tags)
   depends_on = [module.vpc_1, module.vpc_2, aws_lb.nlb]
 }
 
 resource "aws_vpc_endpoint" "client_endpoint" {
-  vpc_id            = module.vpc_2.vpc_id
-  service_name      = aws_vpc_endpoint_service.service_endpoint.service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = [module.vpc_2.public_subnets ]
+  vpc_id             = module.vpc_2.vpc_id
+  service_name       = aws_vpc_endpoint_service.service_endpoint.service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = [module.vpc_2.public_subnets]
   security_group_ids = [aws_security_group.pvtlnk-sg-2.id]
 
-  tags = merge({"Name" = "client_endpoint"}, var.tags)
+  tags = merge({ "Name" = "client_endpoint" }, var.tags)
 }
 
 resource "aws_vpc_endpoint_connection_accepter" "endpoint_accept" {
