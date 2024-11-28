72b3849b9958fd98170cb466a34eaaedf852875d
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-28 03:55:47 +0200

    updates

--- private-link/ec2.tf
+++ private-link/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_key_pair" "key-1" {
 
 resource "aws_security_group" "pvtlnk-sg-1" {
   provider    = aws.eu-central-1
-  vpc_id = module.vpc_1.vpc_id
+  vpc_id      = module.vpc_1.vpc_id
   name_prefix = "pvtlnk-sg-"
 
   ingress {
@@ -29,11 +29,11 @@ resource "aws_security_group" "pvtlnk-sg-1" {
 resource "aws_instance" "vpc_1_ec2" {
   provider = aws.eu-central-1
 
-  ami                    = data.aws_ami.ubuntu_2204_eu_central-1.id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.key-1.key_name
-  security_group_ids     = [aws_security_group.pvtlnk-sg-1.id]
-  subnet_id              = module.vpc_1.public_subnets[0]
+  ami                = data.aws_ami.ubuntu_2204_eu_central-1.id
+  instance_type      = var.instance_type
+  key_name           = aws_key_pair.key-1.key_name
+  security_group_ids = aws_security_group.pvtlnk-sg-1.id]
+  subnet_id          = module.vpc_1.public_subnets[0]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -52,7 +52,7 @@ resource "aws_key_pair" "key-2" {
 
 resource "aws_security_group" "pvtlnk-sg-2" {
   provider    = aws.eu-west-3
-  vpc_id = module.vpc_2.vpc_id
+  vpc_id      = module.vpc_2.vpc_id
   name_prefix = "pvtlnk-sg-"
 
   ingress {
@@ -75,11 +75,11 @@ resource "aws_security_group" "pvtlnk-sg-2" {
 resource "aws_instance" "vpc_2_ec2" {
   provider = aws.eu-west-3
 
-  ami                    = data.aws_ami.ubuntu_2204_eu_west_3.id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.key-2.key_name
-  security_group_ids     = [aws_security_group.pvtlnk-sg-2.id]
-  subnet_id              = module.vpc_2.public_subnets[0]
+  ami                = data.aws_ami.ubuntu_2204_eu_west_3.id
+  instance_type      = var.instance_type
+  key_name           = aws_key_pair.key-2.key_name
+  security_group_ids = [aws_security_group.pvtlnk-sg-2.id]
+  subnet_id          = module.vpc_2.public_subnets[0]
 
   user_data = <<-EOF
               #!/bin/bash
