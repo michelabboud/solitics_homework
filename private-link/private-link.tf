983fc43dc9164f8193ea3fc6ff9ac542ec51ae90
Michel Abboud <michelabboud@users.noreply.github.com>
2024-11-28 08:21:49 +0200

    updates

--- private-link/private-link.tf
+++ private-link/private-link.tf
@@ -2,7 +2,7 @@ resource "aws_vpc_endpoint_service" "service_endpoint" {
 
   provider = aws.eu-central-1
 
-  acceptance_required        = true
+  acceptance_required        = false
   network_load_balancer_arns = [aws_lb.nlb.arn]
 
   allowed_principals = [
